848a172e5ca3f48ee9dbd47de60838b80c902c6c
zhanghua <964552300@qq.com>
2021-02-23 21:51:23 +0800

    spring 初始化项目

---
+++ src/test/java/com/yzu/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzu.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
