cbe68674f9fca78091b96f81d4199448f6253291
zhanghua <964552300@qq.com>
2021-03-04 17:04:23 +0800

    1. 点击sidebar菜单实现页面跳转并增加激活样式

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -356,16 +356,17 @@
         </div><!-- /.sidebar-shortcuts -->
 
         <ul class="nav nav-list">
-          <li class="">
-            <a href="index.html">
+          <li class="" id="welcome-sidebar">
+            <router-link to="/admin/welcome">
               <i class="menu-icon fa fa-tachometer"></i>
               <span class="menu-text"> 欢迎 </span>
-            </a>
+            </router-link>
 
             <b class="arrow"></b>
           </li>
 
-
+<!--          系统管理-->
+<!--          加上了样 式class属性-->
           <li class="">
             <a href="#" class="dropdown-toggle">
               <i class="menu-icon fa fa-list"></i>
@@ -394,6 +395,30 @@
 
                 <b class="arrow"></b>
               </li>
+            </ul>
+          </li>
+
+<!--          业务管理-->
+          <li class="active open">
+            <a href="#" class="dropdown-toggle">
+              <i class="menu-icon fa fa-list"></i>
+              <span class="menu-text"> 业务管理 </span>
+
+              <b class="arrow fa fa-angle-down"></b>
+            </a>
+
+            <b class="arrow"></b>
+
+            <ul class="submenu">
+              <li class="active" id="business-chapter-sidebar">
+                <router-link to="/admin/business/chapter">
+                  <i class="menu-icon fa fa-caret-right"></i>
+                  大章管理
+                </router-link>
+
+                <b class="arrow"></b>
+              </li>
+
             </ul>
           </li>
 
@@ -463,7 +488,27 @@ export default {
   methods:{
     login(){
       this.$router.push("/admin")
-    }
+    },
+
+    /**
+     * 菜单激活样式，id是当前点击的菜单的id
+     * @param id
+     */
+    activeSidebar: function (id) {
+      // 兄弟菜单去掉active样式，自身增加active样式
+      $("#" + id).siblings().removeClass("active");
+      $("#" + id).siblings().find("li").removeClass("active");
+      $("#" + id).addClass("active");
+
+      // 如果有父菜单，父菜单的兄弟菜单去掉open active，父菜单增加open active
+      let parentLi = $("#" + id).parents("li");
+      if (parentLi) {
+        parentLi.siblings().removeClass("open active");
+        parentLi.siblings().find("li").removeClass("active");
+        parentLi.addClass("open active");
+      }
+    },
+
   }
 }
 </script>
--- admin/src/views/admin/welcome.vue
+++ admin/src/views/admin/welcome.vue
@@ -3,3 +3,15 @@
     <h1>你好！！！！</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: "welcome",
+  mounted:function (){
+   this.$parent.activeSidebar("welcome-sidebar")
+  },
+  methods:{
+
+  }
+}
+</script>
