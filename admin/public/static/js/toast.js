415492a1bdf3d0312078f81dce1d7e7068969f31
zhanghua <964552300@qq.com>
2021-05-03 17:32:03 +0800

    1. 修改Toast组件的显示效果 2. 制作Confirm组件

--- admin/public/static/js/toast.js
+++ admin/public/static/js/toast.js
@@ -1,33 +1,29 @@
-const Toast = Swal.mixin({
-    toast: true,
-    position: 'top-end',
-    showConfirmButton: false,
-    timer: 3000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-        toast.addEventListener('mouseenter', Swal.stopTimer)
-        toast.addEventListener('mouseleave', Swal.resumeTimer)
-    }
-})
-
-
-toast = {
-    success: function (message){
-        Toast.fire({
+Toast = {
+    success: function (message) {
+        Swal.fire({
+            position: 'top-end',
             icon: 'success',
-            title: message
+            title: message,
+            showConfirmButton: false,
+            timer: 3000
         })
     },
-    error: function (message){
-        Toast.fire({
+    error: function (message) {
+        Swal.fire({
+            position: 'top-end',
             icon: 'error',
-            title: message
+            title: message,
+            showConfirmButton: false,
+            timer: 3000
         })
     },
-    warning: function (message){
-        Toast.fire({
+    warning: function (message) {
+        Swal.fire({
+            position: 'top-end',
             icon: 'warning',
-            title: message
+            title: message,
+            showConfirmButton: false,
+            timer: 3000
         })
     }
 }
