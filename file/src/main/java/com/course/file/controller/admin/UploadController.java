ce5c872685132596ce98b9fa4ae64bd3242c0d8c
zhanghua <964552300@qq.com>
2021-05-10 23:57:47 +0800

    1. 大文件端点续传与激素秒传：分片合并测试成功

--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
@@ -40,7 +43,7 @@ public class UploadController {
 	private FileService fileService;
 
 	@RequestMapping("/upload")
-	public ResponseDto upload(@RequestParam MultipartFile file,String use) throws IOException {
+	public ResponseDto upload(@RequestParam MultipartFile file, String use) throws IOException {
 		LOG.info("上传文件开始");
 		LOG.info(file.getOriginalFilename());
 		LOG.info(String.valueOf(file.getSize()));
@@ -54,11 +57,11 @@ public class UploadController {
 		//如果文件不存在则创建
 		String dir = useEnum.name().toLowerCase();
 		File fullDir = new File(FILE_PATH + dir);
-		if(!fullDir.exists()){
+		if (!fullDir.exists()) {
 			fullDir.mkdir();
 		}
 
-		String path = dir + File.separator +  key + "." + suffix;
+		String path = dir + File.separator + key + "." + suffix;
 		String fullPath = FILE_PATH + path;
 		File dest = new File(fullPath);
 		file.transferTo(dest);
@@ -77,6 +80,90 @@ public class UploadController {
 		fileDto.setPath(FILE_DOMAIN + path);
 		responseDto.setContent(fileDto);
 
- 		return responseDto;
+		return responseDto;
 	}
+
+	@GetMapping("/merge")
+	public ResponseDto merge(FileDto fileDto) throws Exception {
+		File newFile = new File(FILE_PATH + "/course/test123.mp4");
+		FileOutputStream outputStream = new FileOutputStream(newFile, true);
+		FileInputStream fileInputStream = null;
+		byte[] byt = new byte[10 * 1024 * 1024];
+		int len;
+
+		try {
+			fileInputStream = new FileInputStream(new File(FILE_PATH + "course/mZvcNKmi.blob"));
+			while ((len = fileInputStream.read(byt)) != -1) {
+				outputStream.write(byt, 0, len);
+			}
+
+			//读取第二个分片
+			fileInputStream = new FileInputStream(new File(FILE_PATH + "/course/Fb4cbKnX.blob"));
+			while ((len = fileInputStream.read(byt)) != -1) {
+				outputStream.write(byt, 0, len);
+			}
+		} catch (IOException e) {
+			LOG.info("分片合并异常", e);
+		} finally {
+			try {
+				if (fileInputStream != null) {
+					fileInputStream.close();
+				}
+				outputStream.close();
+				LOG.info("IO流关闭");
+			} catch (Exception e) {
+				LOG.error("IO流关闭", e);
+			}
+			ResponseDto responseDto = new ResponseDto();
+			return responseDto;
+		}
+	}
+
+//	public void merge(FileDto fileDto) throws Exception {
+//		LOG.info("合并分片开始");
+//		String path = fileDto.getPath(); //http://127.0.0.1:9000/file/f/course\6sfSqfOwzmik4A4icMYuUe.mp4
+//		path = path.replace(FILE_DOMAIN, ""); //course\6sfSqfOwzmik4A4icMYuUe.mp4
+//		Integer shardTotal = fileDto.getShardTotal();
+//		File newFile = new File(FILE_PATH + path);
+//		FileOutputStream outputStream = new FileOutputStream(newFile, true);//文件追加写入
+//		FileInputStream fileInputStream = null;//分片文件
+//		byte[] byt = new byte[10 * 1024 * 1024];
+//		int len;
+//
+//		try {
+//			for (int i = 0; i < shardTotal; i++) {
+//				// 读取第i个分片
+//				fileInputStream = new FileInputStream(new File(FILE_PATH + path + "." + (i + 1))); //  course\6sfSqfOwzmik4A4icMYuUe.mp4.1
+//				while ((len = fileInputStream.read(byt)) != -1) {
+//					outputStream.write(byt, 0, len);
+//				}
+//			}
+//		} catch (IOException e) {
+//			LOG.error("分片合并异常", e);
+//		} finally {
+//			try {
+//				if (fileInputStream != null) {
+//					fileInputStream.close();
+//				}
+//				outputStream.close();
+//				LOG.info("IO流关闭");
+//			} catch (Exception e) {
+//				LOG.error("IO流关闭", e);
+//			}
+//		}
+//		LOG.info("合并分片结束");
+//
+//		System.gc();
+//		Thread.sleep(100);
+//
+//		// 删除分片
+//		LOG.info("删除分片开始");
+//		for (int i = 0; i < shardTotal; i++) {
+//			String filePath = FILE_PATH + path + "." + (i + 1);
+//			File file = new File(filePath);
+//			boolean result = file.delete();
+//			LOG.info("删除{}，{}", filePath, result ? "成功" : "失败");
+//		}
+//		LOG.info("删除分片结束");
+//	}
 }
