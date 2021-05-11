35ddc4d8e49abc0774a07af3965085fbeedfa93e
zhanghua <964552300@qq.com>
2021-05-12 00:10:11 +0800

    1. 用户管理与登录：增加重置密码功能，编辑用户信息的时候不修改密码

--- server/src/main/java/com/course/server/service/UserService.java
+++ server/src/main/java/com/course/server/service/UserService.java
@@ -86,7 +86,8 @@ public class UserService {
 	 * @param user
 	 */
 	private void update(User user) {
-		userMapper.updateByPrimaryKey(user);
+		user.setPassword(null);
+		userMapper.updateByPrimaryKeySelective(user);
 	}
 
 	/**
@@ -104,5 +105,17 @@ public class UserService {
 			return userList.get(0);
 		}
 
+	}
+
+
+	/**
+	 * 重置密码
+	 * @param userDto
+	 */
+	public void savePassword(UserDto userDto) {
+		User user = new User();
+		user.setId(userDto.getId());
+		user.setPassword(userDto.getPassword());
+		userMapper.updateByPrimaryKeySelective(user);
 	}
 }
