a09e5715845ac7ecd757a1064f397823c88af9e1
zhanghua <964552300@qq.com>
2021-05-13 03:49:51 +0800

    1. 用户管理与登录：在请求headers中统一增加token

--- admin/src/main.js
+++ admin/src/main.js
@@ -11,16 +11,21 @@ Vue.prototype.$ajax = axios;
 axios.defaults.withCredentials = true;
 
 /**
- * 拦截器
+ * axios拦截器
  */
 axios.interceptors.request.use(function (config) {
-    console.log("请求:", config);
+    console.log("请求：", config);
+    let token = Tool.getLoginUser().token;
+    if (Tool.isNotEmpty(token)) {
+        config.headers.token = token;
+        console.log("请求headers增加token:", token);
+    }
     return config;
 }, error => {
 });
-axios.interceptors.response.use(function (responese) {
-    console.log("返回结果:", responese);
-    return responese;
+axios.interceptors.response.use(function (response) {
+    console.log("返回结果：", response);
+    return response;
 }, error => {
 });
 
