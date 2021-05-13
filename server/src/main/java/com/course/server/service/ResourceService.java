340ab07b6aa34347682ca48e0d1c3bdcb84c8ede
zhanghua <964552300@qq.com>
2021-05-14 01:25:57 +0800

    1. 通用权限管理：资源数的保存

--- server/src/main/java/com/course/server/service/ResourceService.java
+++ server/src/main/java/com/course/server/service/ResourceService.java
@@ -1,5 +1,6 @@
 package com.course.server.service;
 
+import com.alibaba.fastjson.JSON;
 import com.course.server.domain.Resource;
 import com.course.server.domain.ResourceExample;
 import com.course.server.dto.ResourceDto;
@@ -10,18 +11,27 @@ import com.course.server.util.UuidUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.mysql.cj.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service("ResourceService")
 public class ResourceService {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ResourceService.class);
+
 
 	@javax.annotation.Resource
 	private ResourceMapper resourceMapper;
 
 	/**
 	 * 查询列表
+	 *
 	 * @param pageDto
 	 */
 	public void list(PageDto pageDto) {
@@ -39,6 +49,7 @@ public class ResourceService {
 
 	/**
 	 * 插入或保存
+	 *
 	 * @param resourceDto
 	 */
 	public void save(ResourceDto resourceDto) {
@@ -52,6 +63,7 @@ public class ResourceService {
 
 	/**
 	 * 删除
+	 *
 	 * @param id
 	 */
 	public void delete(String id) {
@@ -64,7 +76,6 @@ public class ResourceService {
 	 * @param resource
 	 */
 	private void insert(Resource resource) {
-		resource.setId(UuidUtil.getShortUuid());
 		resourceMapper.insert(resource);
 	}
 
@@ -75,5 +86,82 @@ public class ResourceService {
 	 */
 	private void update(Resource resource) {
 		resourceMapper.updateByPrimaryKey(resource);
+	}
+
+
+	/**
+	 * 保存资源树
+	 *
+	 * @param json
+	 */
+	@Transactional
+	public void saveJson(String json) {
+		List<ResourceDto> jsonList = JSON.parseArray(json, ResourceDto.class);
+		List<ResourceDto> list = new ArrayList<>();
+		if (!CollectionUtils.isEmpty(jsonList)) {
+			for (ResourceDto d : jsonList) {
+				d.setParent("");
+				add(list, d);
+			}
+		}
+		LOG.info("共{}条", list.size());
+
+		resourceMapper.deleteByExample(null);
+		for (int i = 0; i < list.size(); i++) {
+			this.insert(CopyUtil.copy(list.get(i), Resource.class));
+		}
+	}
+
+	/**
+	 * 递归，将树型结构的节点全部取出来，放到list
+	 *
+	 * @param list
+	 * @param dto
+	 */
+	public void add(List<ResourceDto> list, ResourceDto dto) {
+		list.add(dto);
+		if (!CollectionUtils.isEmpty(dto.getChildren())) {
+			for (ResourceDto d : dto.getChildren()) {
+				d.setParent(dto.getId());
+				add(list, d);
+			}
+		}
+	}
+
+	/**
+	 * 按约定将列表转成树
+	 * 要求：ID要正序排列
+	 *
+	 * @return
+	 */
+	public List<ResourceDto> loadTree() {
+		ResourceExample example = new ResourceExample();
+		example.setOrderByClause("id asc");
+		List<Resource> resourceList = resourceMapper.selectByExample(example);
+		List<ResourceDto> resourceDtoList = CopyUtil.copyList(resourceList, ResourceDto.class);
+		for (int i = resourceDtoList.size() - 1; i >= 0; i--) {
+			// 当前要移动的记录
+			ResourceDto child = resourceDtoList.get(i);
+
+			// 如果当前节点没有父节点，则不用往下了
+			if (org.springframework.util.StringUtils.isEmpty(child.getParent())) {
+				continue;
+			}
+			// 查找父节点
+			for (int j = i - 1; j >= 0; j--) {
+				ResourceDto parent = resourceDtoList.get(j);
+				if (child.getParent().equals(parent.getId())) {
+					if (CollectionUtils.isEmpty(parent.getChildren())) {
+						parent.setChildren(new ArrayList<>());
+					}
+					// 添加到最前面，否则会变成倒序，因为循环是从后往前循环的
+					parent.getChildren().add(0, child);
+
+					// 子节点找到父节点后，删除列表中的子节点
+					resourceDtoList.remove(child);
+				}
+			}
+		}
+		return resourceDtoList;
 	}
 }
